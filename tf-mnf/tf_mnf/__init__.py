89db6123a21de58f50c632a2cd45502a51c24457
Javier <37146054+JavierOrjuela@users.noreply.github.com>
2025-05-11 14:54:49 +0100

    Delete tf-mnf/tf_mnf directory

--- tf-mnf/tf_mnf/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-
-
-ROOT = os.getcwd().split("/tf_mnf", 1)[0]
--- tf-mnf/tf_mnf/layers/__init__.py
+++
@@ -1,2 +0,0 @@
-from tf_mnf.layers.mnf_conv import MNFConv2D as MNFConv2D
-from tf_mnf.layers.mnf_dense import MNFDense as MNFDense
